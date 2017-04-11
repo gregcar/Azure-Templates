e950ace3f8f30686f38a5aa9eb77c0dae09bce8e
Greg Carnie <gregcar@microsoft.com>
2017-04-10 18:37:16 -0600

    Missing }

--- windows-vm-o365-hub/scripts/DeployO365SilentWithVersion.ps1
+++ windows-vm-o365-hub/scripts/DeployO365SilentWithVersion.ps1
@@ -30,3 +30,5 @@ $targetFilePath = "$env:temp\configuration.xml"
 Generate-ODTConfigurationXml -Languages AllInUseLanguages -TargetFilePath $targetFilePath | Set-ODTAdd -Version $NULL | Set-ODTDisplay -AcceptEULA $true -Level None | Install-OfficeClickToRun -OfficeVersion $OfficeVersion
 
 # Configuration.xml file for Click-to-Run for Office 365 products reference. https://technet.microsoft.com/en-us/library/JJ219426.aspx
+
+}
