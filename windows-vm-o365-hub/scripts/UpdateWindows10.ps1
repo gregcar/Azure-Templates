a37167cfe37cb92e448e555d40420233cd09ed10
Greg Carnie <gregcar@microsoft.com>
2017-04-11 14:28:21 -0600

    Removed update configuration.

--- windows-vm-o365-hub/scripts/UpdateWindows10.ps1
+++ windows-vm-o365-hub/scripts/UpdateWindows10.ps1
@@ -4,3 +4,8 @@ Process {
   Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d
   Get-WUInstall –MicrosoftUpdate –AcceptAll –AutoReboot
 }
+
+# { "name": "UpdateWindows", "type": "extensions", "location": "[resourceGroup().location]", "apiVersion": "2017-03-30", "dependsOn": [ "[concat('Microsoft.Compute/virtualMachines/', parameters('vmName'))]"],
+# "tags": { "displayName": "UpdateWindows" },
+# "properties": { "publisher": "Microsoft.Compute", "type": "CustomScriptExtension", "typeHandlerVersion": "1.8", "autoUpgradeMinorVersion": true, 
+# "settings": { "fileUris": ["[concat(parameters('_artifactsLocation'), parameters('updateWindowsScriptFileName'))]"], "commandToExecute": "[concat('powershell -ExecutionPolicy bypass -File ', parameters('updateWindowsScriptFileName'))]" }}},
